4cfd82c268f07c5a214770a8b70de6b1d7ae1f6c
Kurnia Kharisma <kurniakharisma17@gmail.com>
2023-05-08 03:30:07 +0700

    changing the navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 export const Navbar = () => {
   return (
     <div className="absolute top-0 left-0 w-full">
-      <div className="flex items-center p-4 py-8 justify-between">
+      <div className="flex items-center p-5 py-10 justify-between">
         <button>
           <img src="./images/icon-hamburger.svg" alt="" />
         </button>
-        <h2 className="font-bold text-lg text-white">room</h2>
+        <img src="./images/logo.svg" alt="" />
         <div></div>
       </div>
     </div>
