eec5b9fd86ef1f3eb05f9f62e4882f0655613c38
Kurnia Kharisma <kurniakharisma17@gmail.com>
2023-05-08 10:12:02 +0700

    fixing mobile navigation button

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -7,22 +7,28 @@ export const NavigationButtons = ({
 }) => {
   return (
     <div className="absolute z-10 top-[calc(60vh-52px)] left-[calc(100vw-119.33px)]">
-      <button
-        className="bg-black p-4"
-        onClick={() => {
-          onNavigationHandler('previous');
-        }}
-      >
-        <img className="h-5 px-2" src="./images/icon-angle-left.svg" alt="" />
-      </button>
-      <button
-        className="bg-black p-4"
-        onClick={() => {
-          onNavigationHandler('next');
-        }}
-      >
-        <img className="h-5 px-2" src="./images/icon-angle-right.svg" alt="" />
-      </button>
+      <div className="flex">
+        <button
+          className="bg-black p-4"
+          onClick={() => {
+            onNavigationHandler('previous');
+          }}
+        >
+          <img className="h-5 px-2" src="./images/icon-angle-left.svg" alt="" />
+        </button>
+        <button
+          className="bg-black p-4"
+          onClick={() => {
+            onNavigationHandler('next');
+          }}
+        >
+          <img
+            className="h-5 px-2"
+            src="./images/icon-angle-right.svg"
+            alt=""
+          />
+        </button>
+      </div>
     </div>
   );
 };
